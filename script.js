06f80e98e1bf60b4d75c6b164dd5a17805235777
Ronaldo Cysne <ronaldocysne@gmail.com>
2023-07-13 11:44:07 -0300

    Iniciando o desktop

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Site Luciana Otoch");
+
+
+const images_carousel = {
+    img01: ('./assets/projeto01.jpg'),
+    img02: ('./assets/projeto02.jpg'),
+    img03: ('./assets/projeto03.jpg'),
+    img04: ('./assets/projeto04.jpg'),
+}
